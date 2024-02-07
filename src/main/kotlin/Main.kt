4bc79cc1e08a3f92035d034ae75204e9e9f4ba2e
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2024-02-07 14:50:06 +0200

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
-fun main(args: Array<String>) {
+fun main() {
     println("Enter your city:")
 
-    val city = readLine();
-    println("User lives in $city");
+    val city = readLine()
+    println("User lives in $city")
 }
